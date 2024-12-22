be660d8bfc40275cb7c7ccfc48f5dd351491935b
Bazith1 <mohammedbazith@outlook.com>
2024-12-22 17:23:38 +0530

    commit2

--- scripts/terraform-eks.sh
+++ scripts/terraform-eks.sh
@@ -3,8 +3,8 @@ set -e
 
 # Install Terraform
 if ! command -v terraform &> /dev/null; then
-  curl -o /tmp/terraform.zip https://releases.hashicorp.com/terraform/1.6.0/terraform_1.6.0_linux_amd64.zip
-  unzip /tmp/terraform.zip -d /usr/local/bin/
+  sudo curl -o /tmp/terraform.zip https://releases.hashicorp.com/terraform/1.6.0/terraform_1.6.0_linux_amd64.zip
+  sudo unzip /tmp/terraform.zip -d /usr/local/bin/
 fi
 
 # Initialize and Apply Terraform
