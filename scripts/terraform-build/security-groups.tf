463f59367abe6d5c530ce7927352dbf82ca51f89
Bazith1 <mohammedbazith@outlook.com>
2024-12-22 21:12:53 +0530

    commit6

--- scripts/terraform-build/security-groups.tf
+++ scripts/terraform-build/security-groups.tf
@@ -27,24 +27,3 @@ resource "aws_security_group_rule" "all_worker_mgmt_egress" {
   type              = "egress"
   cidr_blocks       = ["0.0.0.0/0"]
 }
-
-# Optional: Add a rule for communication between EKS control plane and worker nodes if required
-resource "aws_security_group_rule" "eks_control_plane_ingress" {
-  description       = "Allow inbound traffic from EKS control plane"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.all_worker_mgmt.id
-  type              = "ingress"
-  cidr_blocks       = ["10.0.0.0/8"]  # Private IP range for internal communication (EKS VPC)
-}
-
-resource "aws_security_group_rule" "eks_worker_to_control_plane" {
-  description       = "Allow inbound traffic from worker nodes to control plane"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.all_worker_mgmt.id
-  type              = "ingress"
-  cidr_blocks       = ["10.0.0.0/8"]  # Ensure worker nodes can reach the control plane
-}
